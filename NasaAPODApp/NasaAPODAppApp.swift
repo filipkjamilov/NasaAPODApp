88869537a60c1d71cd07d6e05bd5215c2e4e2996
filipkjamilov <filipkjamilov@gmail.com>
2022-09-05 19:58:37 +0200

    Project init

---
+++ NasaAPODApp/NasaAPODAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPODAppApp.swift
+//  NasaAPODApp
+//
+//  Created by Filip Kjamilov on 5.9.22.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPODAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
